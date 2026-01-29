3f3aae7ae48d27f32375f9b5f3161a76c39e7745
Tess Bowen <tbowen@ucdavis.edu>
2026-01-29 10:31:11 -0800

    Update page_directory.py

--- lstore/page_directory.py
+++ lstore/page_directory.py
@@ -4,7 +4,15 @@ class PageDirectory:
     def __init__(self):
         pass
 
-    def findPages(self, RID):
+    def findRow(self, RID):
         # given a certain RID, return the correlating indirection column, schema column, 
         # and data columns, with the slot # (basically, which row)
+        pass
+
+    def findWritablePage(self, column):
+        # given a column from a table, return the current writable page
+        pass
+
+    def newPage(self, column):
+        # when a column runs out of space, create a new page for that column
         pass
