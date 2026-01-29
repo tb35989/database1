6752390d8854e16a004c61923f9c923847674377
Tess Bowen <tbowen@ucdavis.edu>
2026-01-28 17:35:18 -0800

    Update page.py

--- lstore/page.py
+++ lstore/page.py
@@ -14,16 +14,3 @@ class Page:
         # let self.num_records = the number of 8-byte records that are stored
         self.num_records += 1
         pass
-
-
-# Notes:
-# Table is column oriented so each page is a column
-# each column is a list of the data
-# have page ranges so like 0-99, 100-199 (makes things faster/easier)
-# ^requirement for this database
-# Page directory: need to map each RID into the physical locations of the data
-# The locations are in the form of a list (page, offset)
-# offset is kind of like the row number, for every individual page
-# 1 page has multiple rows
-
-# if the page runs out of space, create a new page
