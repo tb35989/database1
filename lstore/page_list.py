17b8a244b4d2dcb0b2e1588ae729c7d3e7f68ef2
Tess Bowen <tbowen@ucdavis.edu>
2026-01-29 16:08:55 -0800

    Update page_list.py

--- lstore/page_list.py
+++ lstore/page_list.py
@@ -8,19 +8,21 @@ class PageList:
     # create the pagelist
     def __init__(self):
         self.connectedColumns = []
+        newPage = Page()
+        self.connectedColumns.append(newPage)
+
+    # return the current writeable column
+    def writableColumn(self):
+        return self.connectedColumns[-1]
 
     # write data into the correct writeable column. if the column is full,
     # add a new page to the pagelist
     def write(self, value):
-        if len(self.connectedColumns) > 0 and self.writeableColumn.has_capacity() > 0:
+        if self.writeableColumn.has_capacity() > 0:
             self.writeableColumn.write(value)
         else:
             newPage = Page()
             self.connectedColumns.append(newPage)
             newPage.write(value)
-    
-    # return the current writeable column
-    def writableColumn(self):
-        return self.connectedColumns[-1]
 
     
