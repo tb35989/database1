6ed4d35a6ff7f252e46844bcf0c8e0569f972a19
jwei14 <jslwei@ucdavis.edu>
2026-02-01 10:53:42 -0800

    Update test cases

--- m1_tester.py
+++ m1_tester.py
@@ -36,9 +36,14 @@ print("Insert finished")
 
 # Check inserted records using select query
 for key in records:
+    print('select on', key, ':', records[key])
+
+# Check inserted records using select query
+for key in records:
     # select function will return array of records 
     # here we are sure that there is only one record in t hat array
-    record = query.select(key, 0, [1, 1, 1, 1, 1])[0]
+    # check for retreiving version -1. Should retreive version 0 since only one version exists.
+    record = query.select_version(key, 0, [1, 1, 1, 1, 1], -1)[0]
     error = False
     for i, column in enumerate(record.columns):
         if column != records[key][i]:
@@ -49,28 +54,51 @@ for key in records:
         pass
         # print('select on', key, ':', record)
 
+updated_records = {}
 for key in records:
     updated_columns = [None, None, None, None, None]
+    updated_records[key] = records[key].copy()
     for i in range(2, grades_table.num_columns):
         # updated value
         value = randint(0, 20)
         updated_columns[i] = value
-        # copy record to check
-        original = records[key].copy()
         # update our test directory
-        records[key][i] = value
-        query.update(key, *updated_columns)
-        record = query.select(key, 0, [1, 1, 1, 1, 1])[0]
-        error = False
-        for j, column in enumerate(record.columns):
-            if column != records[key][j]:
-                error = True
-        if error:
-            print('update error on', original, 'and', updated_columns, ':', record, ', correct:', records[key])
-        else:
-            pass
-            # print('update on', original, 'and', updated_columns, ':', record)
-        updated_columns[i] = None
+        updated_records[key][i] = value
+    query.update(key, *updated_columns)
+
+    #check version -1 for record
+    record = query.select_version(key, 0, [1, 1, 1, 1, 1], -1)[0]
+    error = False
+    for j, column in enumerate(record.columns):
+        if column != records[key][j]:
+            print('column:', column, 'records:', records[key][j])
+            error = True
+    if error:
+        print('update error on', records[key], 'and', updated_columns, ':', record, ', correct:', records[key])
+    else:
+        pass
+        # print('update on', original, 'and', updated_columns, ':', record)
+
+    #check version -2 for record
+    record = query.select_version(key, 0, [1, 1, 1, 1, 1], -2)[0]
+    error = False
+    for j, column in enumerate(record.columns):
+        if column != records[key][j]:
+            error = True
+    if error:
+        print('update error on', records[key], 'and', updated_columns, ':', record, ', correct:', records[key])
+    else:
+        pass
+        # print('update on', original, 'and', updated_columns, ':', record)
+    
+    #check version 0 for record
+    record = query.select_version(key, 0, [1, 1, 1, 1, 1], 0)[0]
+    error = False
+    for j, column in enumerate(record.columns):
+        if column != updated_records[key][j]:
+            error = True
+    if error:
+        print('update error on', records[key], 'and', updated_columns, ':', record, ', correct:', updated_records[key])
 
 keys = sorted(list(records.keys()))
 # aggregate on every column 
@@ -78,10 +106,25 @@ for c in range(0, grades_table.num_columns):
     for i in range(0, number_of_aggregates):
         r = sorted(sample(range(0, len(keys)), 2))
         # calculate the sum form test directory
+        # version -1 sum
         column_sum = sum(map(lambda key: records[key][c], keys[r[0]: r[1] + 1]))
-        result = query.sum(keys[r[0]], keys[r[1]], c)
+        result = query.sum_version(keys[r[0]], keys[r[1]], c, -1)
         if column_sum != result:
             print('sum error on [', keys[r[0]], ',', keys[r[1]], ']: ', result, ', correct: ', column_sum)
         else:
             pass
             # print('sum on [', keys[r[0]], ',', keys[r[1]], ']: ', column_sum)
+        # version -2 sum
+        column_sum = sum(map(lambda key: records[key][c], keys[r[0]: r[1] + 1]))
+        result = query.sum_version(keys[r[0]], keys[r[1]], c, -2)
+        if column_sum != result:
+            print('sum error on [', keys[r[0]], ',', keys[r[1]], ']: ', result, ', correct: ', column_sum)
+        else:
+            pass
+        # version 0 sum
+        updated_column_sum = sum(map(lambda key: updated_records[key][c], keys[r[0]: r[1] + 1]))
+        updated_result = query.sum_version(keys[r[0]], keys[r[1]], c, 0)
+        if updated_column_sum != updated_result:
+            print('sum error on [', keys[r[0]], ',', keys[r[1]], ']: ', updated_result, ', correct: ', updated_column_sum)
+        else:
+            pass
--- m3_tester_part_2.py
+++ m3_tester_part_2.py
@@ -19,7 +19,7 @@ records = {}
 
 number_of_records = 1000
 number_of_transactions = 100
-number_of_operations_per_record = 10
+number_of_operations_per_record = 1
 num_threads = 8
 
 keys = []
@@ -31,7 +31,7 @@ for i in range(0, number_of_records):
     key = 92106429 + i
     keys.append(key)
     records[key] = [key, randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20)]
-    # print(records[key])
+    print(records[key])
 
 transaction_workers = []
 transactions = []
@@ -45,21 +45,20 @@ for i in range(num_threads):
 
 
 
-
+updated_records = {}
 # x update on every column
 for j in range(number_of_operations_per_record):
     for key in keys:
         updated_columns = [None, None, None, None, None]
+        updated_records[key] = records[key].copy()
         for i in range(2, grades_table.num_columns):
             # updated value
             value = randint(0, 20)
             updated_columns[i] = value
-            # copy record to check
-            original = records[key].copy()
             # update our test directory
-            records[key][i] = value
-            transactions[key % number_of_transactions].add_query(query.select, grades_table, key, 0, [1, 1, 1, 1, 1])
-            transactions[key % number_of_transactions].add_query(query.update, grades_table, key, *updated_columns)
+            updated_records[key][i] = value
+        transactions[key % number_of_transactions].add_query(query.select, grades_table, key, 0, [1, 1, 1, 1, 1])
+        transactions[key % number_of_transactions].add_query(query.update, grades_table, key, *updated_columns)
 print("Update finished")
 
 
@@ -80,17 +79,67 @@ for i in range(num_threads):
 
 score = len(keys)
 for key in keys:
-    try:
-        correct = records[key]
-        query = Query(grades_table)
-        
-        result = query.select(key, 0, [1, 1, 1, 1, 1])[0].columns
-        if correct != result:
-            print('select error on primary key', key, ':', result, ', correct:', correct)
-            score -= 1
-    except:
-        print('Record Not found', key)
+    correct = records[key]
+    query = Query(grades_table)
+    
+    result = query.select_version(key, 0, [1, 1, 1, 1, 1], -1)[0].columns
+    if correct != result:
+        print('select error on primary key', key, ':', result, ', correct:', correct)
         score -= 1
-print('Score', score, '/', len(keys))
+print('Version -1 Score:', score, '/', len(keys))
+
+v2_score = len(keys)
+for key in keys:
+    correct = records[key]
+    query = Query(grades_table)
+    
+    result = query.select_version(key, 0, [1, 1, 1, 1, 1], -2)[0].columns
+    if correct != result:
+        print('select error on primary key', key, ':', result, ', correct:', correct)
+        v2_score -= 1
+print('Version -2 Score:', v2_score, '/', len(keys))
+if score != v2_score:
+    print('Failure: Version -1 and Version -2 scores must be same')
+
+score = len(keys)
+for key in keys:
+    correct = updated_records[key]
+    query = Query(grades_table)
+    
+    result = query.select_version(key, 0, [1, 1, 1, 1, 1], 0)[0].columns
+    if correct != result:
+        print('select error on primary key', key, ':', result, ', correct:', correct)
+        score -= 1
+print('Version 0 Score:', score, '/', len(keys))
+
+number_of_aggregates = 100
+valid_sums = 0
+for i in range(0, number_of_aggregates):
+    r = sorted(sample(range(0, len(keys)), 2))
+    column_sum = sum(map(lambda x: records[x][0] if x in records else 0, keys[r[0]: r[1] + 1]))
+    result = query.sum_version(keys[r[0]], keys[r[1]], 0, -1)
+    if column_sum == result:
+        valid_sums += 1
+print("Aggregate version -1 finished. Valid Aggregations: ", valid_sums, '/', number_of_aggregates)
+
+v2_valid_sums = 0
+for i in range(0, number_of_aggregates):
+    r = sorted(sample(range(0, len(keys)), 2))
+    column_sum = sum(map(lambda x: records[x][0] if x in records else 0, keys[r[0]: r[1] + 1]))
+    result = query.sum_version(keys[r[0]], keys[r[1]], 0, -2)
+    if column_sum == result:
+        v2_valid_sums += 1
+print("Aggregate version -2 finished. Valid Aggregations: ", v2_valid_sums, '/', number_of_aggregates)
+if valid_sums != v2_valid_sums:
+    print('Failure: Version -1 and Version -2 aggregation scores must be same.')
+
+valid_sums = 0
+for i in range(0, number_of_aggregates):
+    r = sorted(sample(range(0, len(keys)), 2))
+    column_sum = sum(map(lambda x: updated_records[x][0] if x in updated_records else 0, keys[r[0]: r[1] + 1]))
+    result = query.sum_version(keys[r[0]], keys[r[1]], 0, 0)
+    if column_sum == result:
+        valid_sums += 1
+print("Aggregate version 0 finished. Valid Aggregations: ", valid_sums, '/', number_of_aggregates)
 
 db.close()
