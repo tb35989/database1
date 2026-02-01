f53c811c768e7517d9e667f562c1f3da22d8d108
SimonZHU-art <shuoguangzhu@gmail.com>
2026-01-31 23:47:12 -0800

    Test1

--- lstore/index.py
+++ lstore/index.py
@@ -1,28 +1,37 @@
 """
 A data strucutre holding indices for various columns of a table. Key column should be indexd by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
 """
-#Test
+from lstore.table import Table
 class Index:
+    # One index for each table. All our empty initially.
+    #key columns are set to index = 0 by default.
 
     def __init__(self, table):
-        # One index for each table. All our empty initially.
+        self.table = table
         self.indices = [None] *  table.num_columns
-        pass
+
+        
 
     """
     # returns the location of all records with the given value on column "column"
     """
 
     def locate(self, column, value):
-        pass
+        
 
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
     """
 
     def locate_range(self, begin, end, column):
-        pass
+        
 
+  
+  
+  
+  
+  
+  
     """
     # optional: Create index on specific column
     """
@@ -35,4 +44,5 @@ class Index:
     """
 
     def drop_index(self, column_number):
+        self.indices[column_number] = None
         pass
