392a246cdfad523def22c945c4a096a7d626839b
Tess Bowen <tbowen@ucdavis.edu>
2026-02-05 10:39:16 -0800

    changing pagedirectory to pagerange

--- lstore/page_range.py
+++ lstore/page_range.py
@@ -1,44 +1,129 @@
-from lstore.page import Page, BasePage, TailPage
+from lstore.page_list import PageList
 
 class PageRange:
-    def __init__(self, num_columns):
-        self.base_pages = [BasePage(num_columns)]
-        self.tail_pages = [TailPage(num_columns)]
-        self.current_base_index = 0 # keeps track of the writable column
-        self.current_tail_index = 0
+    # pagerange: a list of pagelists that make up a table. think of it like a horizontal
+    # list of pages
+    # contains a list of Base pages and a list of Tail pages. Both can perform the same 
+    # functions, they are just kept differentiated.
+    #are these a matrix?
+    def __init__(self):
+        self.pageRangeBase = []
+        self.pageRangeTail = []
 
-    # Adds user info into columns inside base page (for INSERT)
-    def write_base_record(self, rid, record, rid_column):
-        last_page = self.base_pages[-1]
-        # Checks if there is space in base page
-        if last_page.get_offset == 4096: 
-            self.add_base_page()
-            last_page.append_record(rid, record, rid_column)
-        else:
-            last_page.append_record(rid, record, rid_column) 
-    
-    # Used when previous base page is full
-    def add_base_page(self):
-        if len(self.base_pages) == 16: # might not be necessary since we already check key uniqueness 
-            return False 
-        else:
-            b = BasePage()
-            self.base_pages.append(b)
-            self.current_base_index += 1
+    # given an RID, returns the corresponding columns (not including the RID column), 
+    # unless the RID is not found.
+    # (assumes the first column in the table is RID)
+    # (searches both base and tail records)
+    def findPageLists(self, RID):
+        if self.pageRangeTail[0].find(RID) == "not found":
+            if self.pageRangeBase[0].find(RID) == "not found":
+                return "RID not found"
+            else:
+                return self.pageRangeBase[1:]
+        else: 
+            return self.pageRangeTail[1:]
 
-    # Adds user info into columns inside tail page (for UPDATE)
-    def write_tail_record(self, rid, record, rid_column):
-        last_page = self.tail_pages[-1]
-        # Checks if there is space in tail page
-        if last_page.get_offset == 4096: 
-            self.add_tail_page()
-            last_page.append_record(rid, record, rid_column)
+
+
+    # BASE RECORDS
+
+    # adds the specified number of columns to the table. Doesn't write anything in though.
+    def addColumnBase(self, numColumns):
+        for i in range(numColumns):
+            newColumn = PageList(baseOrTail = True)
+            self.pageRangeBase.append(newColumn)
+
+    # writes specified data into the next available slot in the specified column
+    # returns the slot number
+    def writeValueIntoColumnBase(self, columnID, value):
+        if columnID < len(self.pageRangeBase):
+            return self.pageRangeBase[columnID].write(value)
         else:
-            last_page.append_record(rid, record, rid_column) 
-    
-    # Used when previous tail page is full
-    def add_tail_page(self):
-        t = TailPage()
-        self.tail_pages.append(t)
-        self.current_tail_index += 1
+            return "column ID invalid"
         
+    # given a columnID, returns the corresponding pageList
+    def findColumnBase(self, columnID):
+        if columnID < len(self.pageRangeBase):
+            return self.pageRangeBase[columnID]
+        else:
+            return "column ID invalid"
+    
+    # writes data into a new base record row. assumes the RID 
+    # column is the first column in the table, and then writes the rest of the data (record)
+    # into the next columns
+    # also checks to make sure the row all has the same slot #
+    def write_base_record(self, rid, record, rid_col):
+        slotNumbers = []
+        slotNumbers.append(self.pageRangeBase[rid_col].write(rid))
+        for i in range(len(record)):
+            if i != rid_col:
+                slotNumbers.append(self.pageRange[i].write(record[i]))
+        for i in range(len(slotNumbers) - 1):
+            if slotNumbers[0] != slotNumbers[i + 1]:
+                return "error"
+        return "success"
+
+    # given an RID value, return the corresponding pages that contain the data,
+    # and the slot number that all the data is on (assumes all the data is at the same slot #)
+    # (assumes RID is the first column in the table) (includes RID's page as the first item)
+    # the first value in the list returned is the slot number, then comes the pages in order
+    # (this can be changed around if need be)
+    def getBaseRow(self, rid):
+        location = self.pageRangeBase[0].find(rid)
+        row = []
+        if location == "not found":
+            return "RID not found in this table"
+        else:
+            row.append(location[2]) # the slot # for all the data
+            j = location[1] # j = the index of the page the data is on in the pagelist
+            # so for example, if the data is on the second page of the RID column,
+            # j = 1 (since the first page would be 0)
+            # anyways,
+            for i in range(len(self.pageRangeBase)):
+                row.append(self.pageRangeBase[i].getPage(j))
+
+        
+
+
+    # TAIL RECORDS
+
+    # adds the specified number of columns to the table. Doesn't write anything in though.
+    def addColumnTail(self, numColumns):
+        for i in range(numColumns):
+            newColumn = PageList(baseOrTail = False)
+            self.pageRangeTail.append(newColumn)
+
+    # writes specified data into the next available slot in the specified column
+    # returns the slot number
+    def writeValueIntoColumnTail(self, columnID, value):
+        if columnID < len(self.pageRangeTail):
+            return self.pageRangeTail[columnID].write(value)
+        else:
+            return "column ID invalid"
+
+    # given a columnID, returns the corresponding pageList
+    def findColumnTail(self, columnID):
+        if columnID < len(self.pageRangeTail):
+            return self.pageRangeTail[columnID]
+        else:
+            return "column ID invalid"
+        
+    
+    # given an RID value, return the corresponding pages that contain the data,
+    # and the slot number that all the data is on (assumes all the data is at the same slot #)
+    # (assumes RID is the first column in the table) (includes RID's page as the first item)
+    # the first value in the list returned is the slot number, then comes the pages in order
+    # (this can be changed around if need be)
+    def getTailRow(self, rid):
+        location = self.pageRangeTail[0].find(rid)
+        row = []
+        if location == "not found":
+            return "RID not found in this table"
+        else:
+            row.append(location[2]) # the slot # for all the data
+            j = location[1] # j = the index of the page the data is on in the pagelist
+            # so for example, if the data is on the second page of the RID column,
+            # j = 1 (since the first page would be 0)
+            # anyways,
+            for i in range(len(self.pageRangeTail)):
+                row.append(self.pageRangeTail[i].getPage(j))
